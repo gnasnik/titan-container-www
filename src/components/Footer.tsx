2482d4c76648425ca5fee0c384c889393f5ef6e3
gnasnik <frankffenn@gmail.com>
2023-08-15 19:57:23 +0800

    Home Page :)

---
+++ src/components/Footer.tsx
@@ -0,0 +1,19 @@
+
+const Footer = () => {
+    return (
+        <footer class="footer items-center p-4 bg-neutral text-neutral-content">
+        <div class="items-center grid-flow-col">
+          Titan Container
+          <p>
+            made by{" "}
+            <a href="https://github.com/Filecoin-Titan" target="_blank">
+            Filecoin-Titan
+            </a>
+            . Copyright © 2023 - All right reserved
+          </p>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import { createSignal } from 'solid-js';
+import logo from "../assets/logo.png"
+
+const onConnectWallet = () => {
+
+}
+
+const onClickLogo = () => {
+    
+}
+
+const Header = () => {
+  
+  return (
+    <header class="bg-transparent p-4 fixed top-0 left-0 w-full">
+      <div class="flex justify-between items-center">
+        <div class="flex items-center space-x-2 hover">
+          <img class="w-8 h-8" src={logo.src} alt="Logo" />
+          <h1 class="px-4 text-sm font-extrabold bg-clip-text text-transparent bg-gradient-to-r from-pink-500  to-blue-500">Titan Container</h1>
+        </div>
+        <button class="px-4 middle py-2 bg-blue-500 text-white rounded text-xs h-12 hover:bg-op-80" onClick={onConnectWallet}>
+          Connect Wallet
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
