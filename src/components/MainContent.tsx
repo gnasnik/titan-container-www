fe4d2aa5d3a2f638fb95436e6b2c17060a2325b5
gnasnik <frankffenn@gmail.com>
2023-08-29 15:28:25 +0800

    fix siderbar style classes

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -1,7 +1,7 @@
 
 export default () => {
     return (
-        <div class="bg-base dark:bg-gray-100 w-full">
+        <div class="bg-base dark:bg-gray-100 w-full h-screen">
                 <main class="p-4">Test</main>
         </div>
     )
